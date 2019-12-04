1e67a45d6357947bf15f6a19513de4fba729e069
Thorsten <static79@users.noreply.github.com>
2019-12-04 16:52:47 +0100

    Create ReadBytes.java

---
+++ src/main/java/io/ReadBytes.java
@@ -0,0 +1,24 @@
+package de.tbitss.rtianalise.io;
+
+import java.io.IOException;
+import java.io.RandomAccessFile;
+
+/**
+ * Lesen von Bytes einer beliebigen Datei
+ */
+public class ReadBytes {
+
+    public static byte[] readBytes(String filename) throws IOException {
+        RandomAccessFile rafile = null;
+        try {
+            rafile = new RandomAccessFile(filename, "r");
+            byte[] b = new byte[(int) rafile.length()];
+            rafile.readFully(b);
+            return b;
+        } finally {
+            if (rafile != null) {
+                rafile.close();
+            }
+        }
+    }
+}
