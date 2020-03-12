18286fcf80c34b87154debe253748ff4bb2f6dc2
Thorsten <static79@users.noreply.github.com>
2020-03-12 11:52:58 +0100

    Update FileReader.java
    add some doc

--- src/main/java/io/FileReader.java
+++ src/main/java/io/FileReader.java
@@ -7,13 +7,18 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
- * Lesen einer Text-Datei
+ * Read a plain text file.
  */
 public class FileReader {
 
     private java.io.FileReader fr = null;
     private BufferedReader br = null;
-
+    
+	/**
+	 * Creates file reader object which can read a file line by line or a hole file at once 
+	 * @param filename
+	 * @throws FileNotFoundException
+	 */
     public FileReader(String filename) throws FileNotFoundException {
         initialize(filename);
     }
@@ -22,11 +27,21 @@ public class FileReader {
         this.fr = new java.io.FileReader(filename);
         this.br = new BufferedReader(this.fr);
     }
-
+    
+	/**
+	 * Returns one line from the opened file. Meant to be used in a loop.  
+	 * @return line from file
+	 * @throws IOException
+	 */
     public String readLine() throws IOException {
         return this.br.readLine();
     }
-
+    
+	/**
+	 * Returns the hole file line by line as an List. Be careful with extreme large files.
+	 * @return all lines from file
+	 * @throws IOException
+	 */
     public List<String> readLinesAsList() throws IOException {
         List<String> lines = new ArrayList<String>();
         String line = null;
